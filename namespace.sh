6915db81aa0d39473e39eb569c811a89d7f2e8e6
MahabubArafat <arafatmahabub37@gmail.com>
2023-02-10 10:12:04 +0600

    transfer3

--- namespace.sh
+++ namespace.sh
@@ -8,8 +8,8 @@ sudo ip link add port-1 type veth peer name port-2
 sudo link set port-1 netns ns-1
 sudo link set port-2 netns ns-2
 
-sudo ip netns exec ns-1 ip addr add 10.10.1.1/24 dev port-1
-sudo ip netns exec ns-2 ip addr add 10.10.2.2/24 dev port-2
+sudo ip netns exec ns-1 ip addr add 10.10.0.1/24 dev port-1
+sudo ip netns exec ns-2 ip addr add 10.10.0.2/24 dev port-2
 
 sudo ip netns exec ns-1 ip link set dev port-1 up
 sudo ip netns exec ns-2 ip link set dev port-2 up
